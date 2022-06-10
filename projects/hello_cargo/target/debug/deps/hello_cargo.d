17b22d587987a8854cc4e063fbfdd48568f867f6
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2022-06-09 23:06:04 -0700

    cargo

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\hello_cargo\target\debug\hello_cargo.exe: C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\hello_cargo\src\main.rs
