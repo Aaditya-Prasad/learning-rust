4780221111a2650687b3770ebf3a450fec5daeaa
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2022-06-28 13:29:45 -0700

    more stuff

---
+++ projects/chp5/rectangles/target/debug/deps/rectangles.d
@@ -0,0 +1,5 @@
+C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\chp5\rectangles\target\debug\deps\rectangles.exe: src\main.rs
+
+C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\chp5\rectangles\target\debug\deps\rectangles.d: src\main.rs
+
+src\main.rs:
---
+++ projects/chp5/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\chp5\rectangles\target\debug\rectangles.exe: C:\Users\blaze\OneDrive\Documents\GitHub\learning-rust\projects\chp5\rectangles\src\main.rs
