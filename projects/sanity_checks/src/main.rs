5a2069eadc028d45d1321cf92e8023e3450dea8f
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-02-14 21:11:02 -0800

    structs sanity

--- projects/sanity_checks/src/main.rs
+++ projects/sanity_checks/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
-    check1();
-    check2();
+    // check1();
+    // check2();
+    check3();
 }
 
 //Ownership
@@ -9,7 +10,7 @@ fn check1() {
     let y = &mut x;
     *y += 1;
     println!("{}", x);
-    y += 1; //fails, you need to dereference y
+    // y += 1; //fails, you need to dereference y
     println!("{}", x);
 }
 
@@ -17,4 +18,18 @@ fn check2(){
     let s1 = String::from("hello");
     assert!(s1 == "hello");
     println!("s1 is {}", s1);
+}
+
+//structs
+struct Point {
+    x: i32,
+    y: i32,
+    tag : String,
+}
+
+fn check3() {
+    let p1 = Point { x: 5, y: 10 , tag : "p1".to_string() };
+    let p2 = Point { ..p1 };
+    print!("{}", p1.x); //no error since p1.x implements copy
+    print!("{}", p1.tag); //error since p1.tag does not implement copy
 }
